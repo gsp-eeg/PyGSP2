67cace2a258bdafa88c150884ca30b131067698f
Lucas Cortés <lucascortesg@gmail.com>
2024-06-25 12:11:15 -0400

    upates pygsp2 version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='pygsp2',
-    version='2.0.2',
+    version='2.0.3',
     description='Graph Signal Processing in Python',
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
