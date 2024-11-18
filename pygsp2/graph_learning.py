c7ff7c1716553af57a1c10a5d41071dc1675739e
Lucas Cortés <lucascortesg@gmail.com>
2024-11-18 17:26:28 -0300

    forces Z symmetry

--- pygsp2/graph_learning.py
+++ pygsp2/graph_learning.py
@@ -93,6 +93,7 @@ def graph_log_degree(Z, a=1.0, b=1.0, w_0='zeros', w_max=np.inf, tol=1e-5, maxit
 
     """
     # Transform to vector space
+    Z = (Z + Z.T) / 2
     z = squareform(Z)
     z = z / np.amax(z)
 
